48146074fdc0bf5e45601541bfe1c1a9e8980708
E-Moynul <it23017@mbstu.ac.bd>
2025-11-08 23:23:10 +0600

    Create InvalidDeptException.java

---
+++ Testing Exception Handling/InvalidDeptException.java
@@ -0,0 +1,8 @@
+//it23017
+package com.validation.exceptions;
+
+public class InvalidDeptException extends Exception {
+    public InvalidDeptException(String message) {
+        super(message);
+    }
+}
