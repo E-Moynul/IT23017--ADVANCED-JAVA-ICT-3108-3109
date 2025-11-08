8c124ce9a2d03759dcc6b1ed92007feb8b2a934a
E-Moynul <it23017@mbstu.ac.bd>
2025-11-08 23:23:31 +0600

    Create InvalidAgeException.java

---
+++ Testing Exception Handling/InvalidAgeException.java
@@ -0,0 +1,8 @@
+//it23017
+package com.validation.exceptions;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
